24513a37028aea8c18257c2d674050045194b978
SyrianSoldier <9975545+syriansoldier@user.noreply.gitee.com>
2022-12-05 17:37:57 +0800

    feat: add styles file

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDom from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+
+import '@/styles'
 import App from './App'
 import store from '@/store'
 
